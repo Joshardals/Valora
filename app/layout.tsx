607edaafd85ea0ad8a59e74dce8c9441e3b0dfa0
Joshardals <joshuabamidele219@gmail.com>
2024-01-25 15:17:30 +0100

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
-import type { Metadata } from "next";
-import { Providers } from "./providers";
-import "./globals.css";
-import { rubik } from "./fonts";
+// import type { Metadata } from "next";
+// import { Providers } from "./providers";
+// import "./globals.css";
+// import { rubik } from "./fonts";
 
-export const metadata: Metadata = {
-  title: "Accentify",
-  description:
-    "Accentify, a personal e-commerce project focusing on clothing, accessories, and gadgets.",
-};
+// export const metadata: Metadata = {
+//   title: "Accentify",
+//   description:
+//     "Accentify, a personal e-commerce project focusing on clothing, accessories, and gadgets.",
+// };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en" className={rubik.variable}>
-      <body>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
-}
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en" className={rubik.variable}>
+//       <body>
+//         <Providers>{children}</Providers>
+//       </body>
+//     </html>
+//   );
+// }
