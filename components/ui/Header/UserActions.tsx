91cf905795300f95238b77321925a659b14bd3a3
Joshardals <joshuabamidele219@gmail.com>
2024-01-26 17:43:50 +0100

    valora magic

--- components/ui/Header/UserActions.tsx
+++ components/ui/Header/UserActions.tsx
@@ -1,10 +1,10 @@
-import { CiUser, CiBookmark, CiShoppingCart } from "react-icons/ci";
-export default function UserActions() {
-  return (
-    <div className="flex items-center space-x-4 *:cursor-pointer">
-      <CiUser size={20} aria-label="Profile Icon" />
-      <CiBookmark size={20} aria-label="Wishlist" />
-      <CiShoppingCart size={20} aria-label="Shopping Cart"/>
-    </div>
-  );
-}
+// import { CiUser, CiBookmark, CiShoppingCart } from "react-icons/ci";
+// export default function UserActions() {
+//   return (
+//     <div className="flex items-center space-x-4 *:cursor-pointer">
+//       <CiUser size={20} aria-label="Profile Icon" />
+//       <CiBookmark size={20} aria-label="Wishlist" />
+//       <CiShoppingCart size={20} aria-label="Shopping Cart"/>
+//     </div>
+//   );
+// }
