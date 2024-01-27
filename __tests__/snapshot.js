6f2de80438c71402f1a5b55948633a8733765bd4
Joshardals <joshuabamidele219@gmail.com>
2024-01-27 17:32:33 +0100

    initial commit

--- __tests__/snapshot.js
+++ __tests__/snapshot.js
@@ -1,12 +1,5 @@
 import { render } from '@testing-library/react'
-import Page from "@/app/(root)/page";
 import NavLinks from "@/components/ui/Header/NavLinks";
-
-
-it('renders homepage unchanged', () => {
-    const { container } = render(<Page />)
-    expect(container).toMatchSnapshot()
-})
 
 it(`renders navlinks unchanged`, () => {
     const { container } = render(<NavLinks />)
