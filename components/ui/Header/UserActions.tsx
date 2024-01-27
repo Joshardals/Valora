ca44b4c6900e79cd8f1cb56e41034a35a1f68425
Joshardals <joshuabamidele219@gmail.com>
2024-01-27 12:47:22 +0100

    valora magic

--- components/ui/Header/UserActions.tsx
+++ components/ui/Header/UserActions.tsx
@@ -5,7 +5,7 @@ import NavToggle from "./NavToggle";
 
 export default function UserActions() {
   return (
-    <ul className="relative flex items-center space-x-4 *:cursor-pointer linkList">
+    <ul className="flex items-center space-x-4 *:cursor-pointer linkList">
       <li className="max-md:hidden">
         <PiCurrencyGbpLight size={20} aria-label="Currency" />
       </li>
@@ -16,17 +16,13 @@ export default function UserActions() {
         <CiBookmark size={20} aria-label="Wishlist" />
       </li>
       <li className="md:hidden">
-        <CiSearch
-          size={20}
-          aria-label="Search"
-          className="max-md:min-h-7 max-md:min-w-7 max-md:max-w-7 max-md:max-h-7"
-        />
+        <CiSearch size={20} aria-label="Search" className="max-md:size-7" />
       </li>
       <li>
         <CiShoppingCart
           size={20}
           aria-label="Shopping Cart"
-          className=" max-md:min-h-7 max-md:min-w-7 max-md:max-w-7 max-md:max-h-7"
+          className="max-md:size-7"
         />
       </li>
       <li className="md:hidden  max-h-5 pt-1">
