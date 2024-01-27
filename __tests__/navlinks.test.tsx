d52ba180e1873a0da8c1fd9be586bf82eb3c5361
Joshardals <joshuabamidele219@gmail.com>
2024-01-27 18:33:37 +0100

    initial commit

--- __tests__/navlinks.test.tsx
+++ __tests__/navlinks.test.tsx
@@ -1,17 +1,17 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import NavLinks from "@/components/ui/Header/NavLinks";
-import Link from "next/link";
+// import "@testing-library/jest-dom";
+// import { render, screen } from "@testing-library/react";
+// import NavLinks from "@/components/ui/Header/NavLinks";
+// import Link from "next/link";
 
-describe("NavLinks", () => {
-  it("renders link correctly", () => {
-    render(<NavLinks />);
+// describe("NavLinks", () => {
+//   it("renders link correctly", () => {
+//     render(<NavLinks />);
 
-    const links = screen.getAllByRole("link");
+//     const links = screen.getAllByRole("link");
 
-    // Check for each link's test
-    expect(links[0]).toHaveTextContent("Shop");
-    expect(links[1]).toHaveTextContent("About Us");
-    expect(links[2]).toHaveTextContent("Search");
-  });
-});
+//     // Check for each link's test
+//     expect(links[0]).toHaveTextContent("Shop");
+//     expect(links[1]).toHaveTextContent("About Us");
+//     expect(links[2]).toHaveTextContent("Search");
+//   });
+// });
