14bd56be05d0f73408a8ff183c6042a4bc76973c
Joshardals <joshuabamidele219@gmail.com>
2024-01-28 17:05:50 +0100

    initial commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -4,31 +4,39 @@ import Logo from "../ui/Header/Logo";
 import NavLinks from "../ui/Header/NavLinks";
 import UserActions from "../ui/Header/UserActions";
 import { useScrolled } from "@/lib/store/store";
+import HeaderInfo from "../ui/Header/HeaderInfo";
 
 export default function Header() {
   const { scrolled, setScrolled } = useScrolled();
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const scroll = window.scrollY;
+  useEffect(() => {
+    const handleScroll = () => {
+      const scroll = window.scrollY;
 
-  //     if (scroll > 50) {
-  //       console.log(scroll.toFixed());
-  //     }
-  //   };
+      if (scroll > 50) {
+        console.log(scroll.toFixed(), "Header can now disappear");
+      } else if (scroll < 50) {
+        console.log(scroll.toFixed());
+      }
+    };
 
-  //   window.addEventListener("scroll", handleScroll as any);
-  // }, [window]);
+    window.addEventListener("scroll", handleScroll as any);
+    return () => window.removeEventListener("scroll", handleScroll as any);
+  }, []);
+
   return (
-    <header
-      className="grid grid-cols-3 items-center text-secondary py-5 px-8 max-md:px-5 select-none
-    justify-start"
-    >
-      <Logo />
-      <div className="justify-self-center max-md:hidden">
-        <NavLinks />
-      </div>
-      <div className="justify-self-end max-md:col-span-2">
-        <UserActions />
+    <header>
+      <HeaderInfo />
+      <div
+        className="grid grid-cols-3 items-center text-secondary py-5 px-8 max-md:px-5 select-none
+    justify-start transition-[bg] duration-200 ease-in-out hover:text-primary hover:bg-secondary"
+      >
+        <Logo />
+        <div className="justify-self-center max-md:hidden">
+          <NavLinks />
+        </div>
+        <div className="justify-self-end max-md:col-span-2">
+          <UserActions />
+        </div>
       </div>
     </header>
   );
---
+++ components/ui/Header/HeaderInfo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HeaderInfo() {
+  return (
+    <div className="bg-primary text-secondary text-center p-2">
+      <p className="uppercase text-[0.8rem]">
+        international shipping - duties and taxes included
+      </p>
+    </div>
+  );
+}
