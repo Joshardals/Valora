ae9bc91641599b4c80e46fe53111ac28c94f7356
Joshardals <joshuabamidele219@gmail.com>
2024-01-29 12:17:05 +0100

    initial commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -8,45 +8,18 @@ import { useScroll } from "@/lib/store/store";
 
 export default function Header() {
   const scrollDirection = useScrollDirection();
-  const { scroll } = useScroll();
-
-  // return (
-  //   <header
-  //     className={`sticky grid grid-flow-row ${
-  //       scrollDirection === "down" ? " -top-24" : "top-0"
-  //     } transition-all duration-200 z-10`}
-  //   >
-  //     {/* <HeaderInfo /> */}
-  //     <div
-  //       className={`grid grid-cols-3 items-center max-md:bg-secondary max-md:text-primary py-5 px-8 max-md:px-5 select-none
-  //       justify-start headerHover ${
-  //         scrollDirection === "up" && !scroll
-  //           ? "text-primary bg-secondary"
-  //           : "text-secondary"
-  //       } `}
-  //     >
-  //       <Logo />
-  //       <div className="justify-self-center max-md:hidden">
-  //         <NavLinks />
-  //       </div>
-  //       <div className="justify-self-end max-md:col-span-2">
-  //         <UserActions />
-  //       </div>
-  //     </div>
-  //   </header>
-  // );
 
   return (
     <header
       className={`fixed top-0 w-full grid grid-flow-row
-    ${scroll ? "-top-[100%]" : "top-0"}`}
+      duration-100`}
+      id="header"
     >
       <HeaderInfo />
       <div
-        className={`grid grid-cols-3 items-center max-md:bg-secondary max-md:text-primary py-5 px-8 max-md:px-5 select-none
-        justify-start headerHover  ${
-          scroll ? "bg-secondary text-primary" : "text-secondary"
-        } `}
+        className={`grid grid-cols-3 items-center text-secondary max-md:bg-secondary max-md:text-primary py-5 px-8 max-md:px-5 select-none
+        justify-start headerHover`}
+        id="headerChild"
       >
         <Logo />
         <div className="justify-self-center max-md:hidden">
--- lib/hooks/useScrollDirection.tsx
+++ lib/hooks/useScrollDirection.tsx
@@ -4,44 +4,34 @@ import { useScroll } from "../store/store";
 export default function useScrollDirection() {
   const { scroll, setScroll } = useScroll();
 
-  // useEffect(() => {
-  //   let lastScrollY = 0;
-
-  //   const updateScrollPosition = () => {
-  //     const scrollY = window.scrollY;
-  //     const direction = scrollY > lastScrollY ? "down" : "up";
-
-  //     if (
-  //       direction !== scrollDirection &&
-  //       (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)
-  //     ) {
-  //       setScrollDirection(direction);
-  //     }
-
-  //     lastScrollY = scrollY > 0 ? scrollY : 0;
-  //     if (lastScrollY === 0) {
-  //       setNoScroll(true);
-  //     } else {
-  //       setNoScroll(false);
-  //     }
-  //   };
-
-  //   window.addEventListener("scroll", updateScrollPosition);
-  //   return () => {
-  //     window.removeEventListener("scroll", updateScrollPosition);
-  //   };
-  // }, [scrollDirection]);
-
   useEffect(() => {
     let lastScrollTop = 0;
 
     const updateScrollPosition = () => {
       const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      const header = document.getElementById("header");
+      const headerChild = document.getElementById("headerChild");
 
-      if (scrollTop > lastScrollTop) {
-        setScroll(true);
-      } else {
-        setScroll(false);
+      if (scrollTop > lastScrollTop && scrollTop !== 0) {
+        if (header && headerChild) {
+          header.style.top = "-20%";
+
+          headerChild.style.backgroundColor = "#F7F1EC";
+          headerChild.style.color = "#2C231D";
+        }
+      } else if (scrollTop < lastScrollTop && scrollTop !== 0) {
+        if (header && headerChild) {
+          header.style.top = "0";
+
+          headerChild.style.backgroundColor = "#F7F1EC";
+          headerChild.style.color = "#2C231D";
+        }
+      } else if (scrollTop === 0) {
+        if (header && headerChild) {
+          header.style.top = "0";
+          headerChild.style.backgroundColor = "transparent";
+          headerChild.style.color = "white";
+        }
       }
 
       lastScrollTop = scrollTop;
@@ -51,7 +41,7 @@ export default function useScrollDirection() {
     return () => {
       window.removeEventListener("scroll", updateScrollPosition);
     };
-  }, []);
+  });
 
   return scroll;
 }
