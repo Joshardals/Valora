f94577cac94d06384e4b0d135d5f86da73ec9635
Joshardals <joshuabamidele219@gmail.com>
2024-01-29 19:33:19 +0100

    initial commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({
     <html>
       <body>
         <Header />
-        <div className=" pt-32 px-5 text-wrap">
+        <div className=" px-5 text-wrap">
           {children}
           <SpeedInsights />
         </div>
