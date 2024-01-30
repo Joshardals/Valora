43764362702e3fed7f516ba77eb6f9d32c9759ce
Joshardals <joshuabamidele219@gmail.com>
2024-01-30 18:19:40 +0100

    initial commit

--- components/ui/Header/UserActionsSideBar.tsx
+++ components/ui/Header/UserActionsSideBar.tsx
@@ -1,28 +1,28 @@
-import { TfiClose } from "react-icons/tfi";
-import { userActionsSideBarToggle } from "@/lib/store/store";
-import UserActions from "./UserActions";
+// import { TfiClose } from "react-icons/tfi";
+// import { userActionsSideBarToggle } from "@/lib/store/store";
+// import UserActions from "./UserActions";
 
-export default function UserActionsSideBar() {
-  const { isOpen, setIsOpen } = userActionsSideBarToggle();
-  return (
-    <aside
-      className={`fixed text-secondary top-0 right-0 bg-primary/50 backdrop-blur-lg p-5
-      min-h-[100svh] w-[28rem] transition-transform duration-700
-      ${isOpen ? "-translate-x-0" : "translate-x-full"}
-      `}
-    >
-      <section className="flex items-center justify-between">
-        <UserActions />
-        <TfiClose
-          aria-label="Close Sidebar"
-          className="cursor-pointer"
-          onClick={() => setIsOpen(false)}
-        />
-      </section>
+// export default function UserActionsSideBar() {
+//   const { isOpen, setIsOpen } = userActionsSideBarToggle();
+//   return (
+//     <aside
+//       className={`fixed text-secondary top-0 right-0 bg-primary/50 backdrop-blur-lg p-5
+//       min-h-[100svh] w-[28rem] transition-transform duration-700
+//       ${isOpen ? "-translate-x-0" : "translate-x-full"}
+//       `}
+//     >
+//       <section className="flex items-center justify-between">
+//         <UserActions />
+//         <TfiClose
+//           aria-label="Close Sidebar"
+//           className="cursor-pointer"
+//           onClick={() => setIsOpen(false)}
+//         />
+//       </section>
 
-      <section>
-        <div>Login Page</div>
-      </section>
-    </aside>
-  );
-}
+//       <section>
+//         <div>Login Page</div>
+//       </section>
+//     </aside>
+//   );
+// }
