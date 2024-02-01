90c50c27e0c78c354af50bf1b6c4173b24b446ff
Joshardals <joshuabamidele219@gmail.com>
2024-02-01 20:02:42 +0100

    initial commit

--- components/ui/UserActionSideBar/UserActionsLogin.tsx
+++ components/ui/UserActionSideBar/UserActionsLogin.tsx
@@ -1,34 +1,34 @@
-import LoginForm from "@/components/form/LoginForm";
-import {
-  userActionActiveIndex,
-  userActionsSideBarToggle,
-} from "@/lib/store/store";
-import Link from "next/link";
+// import LoginForm from "@/components/form/LoginForm";
+// import {
+//   userActionActiveIndex,
+//   userActionsSideBarToggle,
+// } from "@/lib/store/store";
+// import Link from "next/link";
 
-export default function UserActionsLogin() {
-  const { isOpen } = userActionsSideBarToggle();
-  const { activeIndex } = userActionActiveIndex();
-  return (
-    <div
-      className={`px-8 space-y-12 ${
-        isOpen ? "fadeIn" : isOpen !== null && "fadeOut"
-      } ${activeIndex === 1 ? "block" : "hidden"}`}
-    >
-      <div className="space-y-4">
-        <h2>Log In</h2>
-        <p className=" font-thin text-pretty max-w-full">
-          Don't have an account?{" "}
-          <span>
-            <Link href="/register" className="linkHover">
-              Create an account
-            </Link>{" "}
-            to unlock your order history, checkout faster, and protect your
-            Wishlist.
-          </span>
-        </p>
-      </div>
+// export default function UserActionsLogin() {
+//   const { isOpen } = userActionsSideBarToggle();
+//   const { activeIndex } = userActionActiveIndex();
+//   return (
+//     <div
+//       className={`px-8 space-y-12 ${
+//         isOpen ? "fadeIn" : isOpen !== null && "fadeOut"
+//       } ${activeIndex === 1 ? "block" : "hidden"}`}
+//     >
+//       <div className="space-y-4">
+//         <h2>Log In</h2>
+//         <p className=" font-thin text-pretty max-w-full">
+//           Don't have an account?{" "}
+//           <span>
+//             <Link href="/register" className="linkHover">
+//               Create an account
+//             </Link>{" "}
+//             to unlock your order history, checkout faster, and protect your
+//             Wishlist.
+//           </span>
+//         </p>
+//       </div>
 
-      <LoginForm />
-    </div>
-  );
-}
+//       <LoginForm />
+//     </div>
+//   );
+// }
