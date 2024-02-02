e342b055ee3cd8e99253ed31f3353f44a4ed9bc2
Joshardals <joshuabamidele219@gmail.com>
2024-02-02 17:48:22 +0100

    initial commit

--- components/ui/Elements/Input.tsx
+++ components/ui/Elements/Input.tsx
@@ -1,16 +1,16 @@
-import { InputProps } from "@/typings";
+// import { InputProps } from "@/typings";
 
-export default function Input({ placeholder, type }: InputProps) {
-  return (
-    <input
-      aria-label="input"
-      autoComplete={type === "password" ? "new-password" : "off"}
-      className="
-    bg-transparent outline-none border-b border-secondary py-2 w-full
-     placeholder:text-secondary placeholder:select-none
-  "
-      placeholder={placeholder}
-      type={type}
-    />
-  );
-}
+// export default function Input({ placeholder, type }: InputProps) {
+//   return (
+//     <input
+//       aria-label="input"
+//       autoComplete={type === "password" ? "new-password" : "off"}
+//       className="
+//     bg-transparent outline-none border-b border-secondary py-2 w-full
+//      placeholder:text-secondary placeholder:select-none
+//   "
+//       placeholder={placeholder}
+//       type={type}
+//     />
+//   );
+// }
