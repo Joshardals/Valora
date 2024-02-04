44d87093cc0ed0ddba66793840556cbb719c9c3a
Joshardals <joshuabamidele219@gmail.com>
2024-02-04 15:13:02 +0100

    initial commit

--- components/ui/Elements/Input.tsx
+++ components/ui/Elements/Input.tsx
@@ -1,24 +1,24 @@
-import { InputProps } from "@/typings";
+// import { InputProps } from "@/typings";
 
-export default function Input({ placeholder, type }: InputProps) {
-  return (
-    <div className="relative">
-      <input
-        aria-label="input"
-        autoComplete={type === "password" ? "new-password" : "false"}
-        className={`
-        bg-transparent outline-none border-b border-secondary py-2 w-full
-      placeholder:text-secondary placeholder:select-none ${
-        type === "password" && "text-[0.6rem]"
-      }
-        `}
-        type={type}
-        placeholder=""
-        required
-      />
-      <span className=" absolute top-2 left-0 duration-200 ease-in floating-label pointer-events-none">
-        {placeholder}
-      </span>
-    </div>
-  );
-}
+// export default function Input({ placeholder, type }: InputProps) {
+//   return (
+//     <div className="relative">
+//       <input
+//         aria-label="input"
+//         autoComplete={type === "password" ? "new-password" : "false"}
+//         className={`
+//         bg-transparent outline-none border-b border-secondary py-2 w-full
+//       placeholder:text-secondary placeholder:select-none ${
+//         type === "password" && "text-[0.6rem]"
+//       }
+//         `}
+//         type={type}
+//         placeholder=""
+//         required
+//       />
+//       <span className=" absolute top-2 left-0 duration-200 ease-in floating-label pointer-events-none">
+//         {placeholder}
+//       </span>
+//     </div>
+//   );
+// }
