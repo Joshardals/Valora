0751ae939927424cffb4041692433f867036b30e
Joshardals <joshuabamidele219@gmail.com>
2024-02-05 22:28:15 +0100

    initial commit

--- components/form/LoginForm.tsx
+++ components/form/LoginForm.tsx
@@ -1,22 +1,22 @@
-// import Link from "next/link";
-// import Button from "../ui/Elements/Button";
-// import Input from "../ui/Elements/Input";
-// import { FormEvent } from "react";
+import Link from "next/link";
+import Button from "../ui/Elements/Button";
+import Input from "../ui/Elements/Input";
+import { FormEvent } from "react";
 
-// export default function LoginForm() {
-//   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     alert("Form Submitted!");
-//   };
-//   return (
-//     <form onSubmit={handleSubmit} className="space-y-12" autoComplete="off">
-//       <Input placeholder="EMAIL*" type="email" />
-//       <Input placeholder="PASSWORD*" type="password" />
+export default function LoginForm() {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert("Form Submitted!");
+  };
+  return (
+    <form onSubmit={handleSubmit} className="space-y-12" autoComplete="off">
+      <Input placeholder="EMAIL*" type="email" />
+      <Input placeholder="PASSWORD*" type="password" />
 
-//       <Button />
-//       <Link href="/register" className="flex justify-center w-full">
-//         <p className="uppercase linkHover font-thin">forgotten password</p>
-//       </Link>
-//     </form>
-//   );
-// }
+      <Button />
+      <Link href="/register" className="flex justify-center w-full">
+        <p className="uppercase linkHover font-thin">forgotten password</p>
+      </Link>
+    </form>
+  );
+}
