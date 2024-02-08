7d147048cb5ebe281d2b7761238379d71fba4e18
Joshardals <joshuabamidele219@gmail.com>
2024-02-08 20:27:17 +0100

    initial commit

--- components/ui/Header/UserActions.tsx
+++ components/ui/Header/UserActions.tsx
@@ -1,50 +1,50 @@
-import { IconItems } from "@/lib/data";
-import NavToggle from "./NavToggle";
-import {
-  userActionActiveIndex,
-  userActionMobileSideBarToggle,
-  userActionSideBarToggle,
-} from "@/lib/store/store";
+// import { IconItems } from "@/lib/data";
+// import NavToggle from "./NavToggle";
+// import {
+//   userActionActiveIndex,
+//   userActionMobileSideBarToggle,
+//   userActionSideBarToggle,
+// } from "@/lib/store/store";
 
-export default function UserActions() {
-  const { activeIndex, setActiveIndex } = userActionActiveIndex();
-  const { isMobileOpen } = userActionMobileSideBarToggle();
-  const { isOpen, setIsOpen } = userActionSideBarToggle();
-  const items = IconItems();
+// export default function UserActions() {
+//   const { activeIndex, setActiveIndex } = userActionActiveIndex();
+//   const { isMobileOpen } = userActionMobileSideBarToggle();
+//   const { isOpen, setIsOpen } = userActionSideBarToggle();
+//   const items = IconItems();
 
-  const handleClick = (index: number) => {
-    setIsOpen(true);
-    setActiveIndex(index);
-  };
+//   const handleClick = (index: number) => {
+//     setIsOpen(true);
+//     setActiveIndex(index);
+//   };
 
-  return (
-    <ul
-      className={`flex items-center max-xs:space-x-3 space-x-4 *:cursor-pointer 
-      ${isOpen ? "activeList" : "linkList"}`}
-    >
-      {items.map((item, index) => {
-        const { label, icon } = item;
-        return (
-          <li
-            key={index}
-            className={` 
-          ${label === "cart" && isOpen && "max-md:hidden"}
-          ${label === "currency" && "max-md:hidden"}
-          ${label === "profile" && "max-md:hidden"}
-          ${label === "wishlist" && "max-md:hidden"}
-          ${label === "search" && "hidden"}
-          ${label === "search" && isOpen ? "block" : ""} 
-          ${index === activeIndex && "active"}
-        `}
-            onClick={() => handleClick(index)}
-          >
-            {icon}
-          </li>
-        );
-      })}
-      <li className="md:hidden max-h-5 pt-1">
-        <NavToggle background={isMobileOpen ? "bg-secondary" : "bg-primary"} />
-      </li>
-    </ul>
-  );
-}
+//   return (
+//     <ul
+//       className={`flex items-center max-xs:space-x-3 space-x-4 *:cursor-pointer 
+//       ${isOpen ? "activeList" : "linkList"}`}
+//     >
+//       {items.map((item, index) => {
+//         const { label, icon } = item;
+//         return (
+//           <li
+//             key={index}
+//             className={` 
+//           ${label === "cart" && isOpen && "max-md:hidden"}
+//           ${label === "currency" && "max-md:hidden"}
+//           ${label === "profile" && "max-md:hidden"}
+//           ${label === "wishlist" && "max-md:hidden"}
+//           ${label === "search" && "hidden"}
+//           ${label === "search" && isOpen ? "block" : ""} 
+//           ${index === activeIndex && "active"}
+//         `}
+//             onClick={() => handleClick(index)}
+//           >
+//             {icon}
+//           </li>
+//         );
+//       })}
+//       <li className="md:hidden max-h-5 pt-1">
+//         <NavToggle background={isMobileOpen ? "bg-secondary" : "bg-primary"} />
+//       </li>
+//     </ul>
+//   );
+// }
