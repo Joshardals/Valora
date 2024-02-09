5b2fe3add75ab614b6374432da492b2a6bed841f
Joshardals <joshuabamidele219@gmail.com>
2024-02-09 22:50:29 +0100

    initial commit

--- components/ui/UserActionSideBar/UserActionsWishList.tsx
+++ components/ui/UserActionSideBar/UserActionsWishList.tsx
@@ -1,54 +1,54 @@
-// import {
-//   userActionActiveIndex,
-//   userActionInitialRender,
-//   userActionSideBarToggle,
-// } from "@/lib/store/store";
-// import Link from "next/link";
-// import { useEffect, useRef } from "react";
+import {
+  userActionActiveIndex,
+  userActionInitialRender,
+  userActionSideBarToggle,
+} from "@/lib/store/store";
+import Link from "next/link";
+import { useEffect, useRef } from "react";
 
-// export default function UserActionsWishList() {
-//   const { activeIndex } = userActionActiveIndex();
-//   const { initialRender, setInitialRender } = userActionInitialRender();
-//   const { isOpen } = userActionSideBarToggle();
-//   const wishlistRef = useRef<HTMLDivElement | null>(null);
+export default function UserActionsWishList() {
+  const { activeIndex } = userActionActiveIndex();
+  const { initialRender, setInitialRender } = userActionInitialRender();
+  const { isOpen } = userActionSideBarToggle();
+  const wishlistRef = useRef<HTMLDivElement | null>(null);
 
-//   useEffect(() => {
-//     if (isOpen) {
-//       if (activeIndex === 2 && initialRender) {
-//         wishlistRef.current?.classList.add("fadeIn");
-//         setInitialRender(false);
-//       } else {
-//         wishlistRef.current?.classList.remove("fadeIn");
-//       }
-//     }
-//   }, [isOpen, activeIndex]);
-//   return (
-//     <div
-//       className={`px-8 space-y-12 ${
-//         isOpen ? "fadeIn" : isOpen !== null && "fadeOut"
-//       } ${activeIndex === 2 ? "block" : "hidden"}`}
-//       ref={wishlistRef}
-//     >
-//       <div className="space-y-4">
-//         <h2>Wishlist is empty</h2>
-//         <p className=" font-thin text-pretty max-w-full">
-//           Save your wishlist by{" "}
-//           <span>
-//             <Link href="/" className="underline">
-//               login
-//             </Link>
-//           </span>{" "}
-//           or creating{" "}
-//           <span>
-//             <Link href="/" className="underline">
-//               your account
-//             </Link>
-//           </span>
-//         </p>
-//         <Link href="/register" className="flex w-full">
-//           <p className="uppercase linkHover font-thin">continue shopping</p>
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// }
+  useEffect(() => {
+    if (isOpen) {
+      if (activeIndex === 2 && initialRender) {
+        wishlistRef.current?.classList.add("fadeIn");
+        setInitialRender(false);
+      } else {
+        wishlistRef.current?.classList.remove("fadeIn");
+      }
+    }
+  }, [isOpen, activeIndex]);
+  return (
+    <div
+      className={`px-8 space-y-12 ${
+        isOpen ? "fadeIn" : isOpen !== null && "fadeOut"
+      } ${activeIndex === 2 ? "block" : "hidden"}`}
+      ref={wishlistRef}
+    >
+      <div className="space-y-4">
+        <h2>Wishlist is empty</h2>
+        <p className=" font-thin text-pretty max-w-full">
+          Save your wishlist by{" "}
+          <span>
+            <Link href="/" className="underline">
+              login
+            </Link>
+          </span>{" "}
+          or creating{" "}
+          <span>
+            <Link href="/" className="underline">
+              your account
+            </Link>
+          </span>
+        </p>
+        <Link href="/register" className="flex w-full">
+          <p className="uppercase linkHover font-thin">continue shopping</p>
+        </Link>
+      </div>
+    </div>
+  );
+}
