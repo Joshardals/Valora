dcce88e7dc0d6e3d0640cbea24d15cd1151d105b
Joshardals <joshuabamidele219@gmail.com>
2024-02-11 19:13:49 +0100

    initial commit

--- lib/data.tsx
+++ lib/data.tsx
@@ -1,48 +1,48 @@
-// // storing data for the UserActions Component Start.
-// import { CiUser, CiBookmark, CiShoppingCart } from "react-icons/ci";
-// import { CiSearch } from "react-icons/ci";
-// import { PiCurrencyGbpLight } from "react-icons/pi";
-// import { useIsMobile } from "./store/store";
-// import useMobile from "./hooks/useMobile";
+// storing data for the UserActions Component Start.
+import { CiUser, CiBookmark, CiShoppingCart } from "react-icons/ci";
+import { CiSearch } from "react-icons/ci";
+import { PiCurrencyGbpLight } from "react-icons/pi";
+import { useIsMobile } from "./store/store";
+import useMobile from "./hooks/useMobile";
 
-// export const IconItems = () => {
-//   const { mobile } = useIsMobile();
-//   useMobile();
+export const IconItems = () => {
+  const { mobile } = useIsMobile();
+  useMobile();
 
-//   const items = [
-//     {
-//       label: "currency",
-//       icon: <PiCurrencyGbpLight size={20} aria-label="Currency" />,
-//     },
-//     {
-//       label: "account",
-//       icon: <CiUser size={20} aria-label="Profile Icon" />,
-//     },
-//     {
-//       label: "wishlist",
-//       icon: <CiBookmark size={20} aria-label="Wishlist" />,
-//     },
-//     {
-//       label: "search",
-//       icon: <CiSearch size={mobile ? 25 : 20} aria-label="Search" />,
-//     },
-//     {
-//       label: "cart",
-//       icon: (
-//         <CiShoppingCart size={mobile ? 25 : 20} aria-label="Shopping Cart" />
-//       ),
-//     },
-//   ];
+  const items = [
+    {
+      label: "currency",
+      icon: <PiCurrencyGbpLight size={20} aria-label="Currency" />,
+    },
+    {
+      label: "account",
+      icon: <CiUser size={20} aria-label="Profile Icon" />,
+    },
+    {
+      label: "wishlist",
+      icon: <CiBookmark size={20} aria-label="Wishlist" />,
+    },
+    {
+      label: "search",
+      icon: <CiSearch size={mobile ? 25 : 20} aria-label="Search" />,
+    },
+    {
+      label: "cart",
+      icon: (
+        <CiShoppingCart size={mobile ? 25 : 20} aria-label="Shopping Cart" />
+      ),
+    },
+  ];
 
-//   return items;
-// };
+  return items;
+};
 
-// // storing data for the UserActions Component End.
+// storing data for the UserActions Component End.
 
-// // Navigation Links that will be used throughout the site.
-// export const links = [
-//   { label: "New In", href: "#" },
-//   { label: "Shop", href: "#" },
-//   { label: "About us", href: "#" },
-//   { label: "Search", href: "#", icon: <CiSearch size={20} /> },
-// ];
+// Navigation Links that will be used throughout the site.
+export const links = [
+  { label: "New In", href: "#" },
+  { label: "Shop", href: "#" },
+  { label: "About us", href: "#" },
+  { label: "Search", href: "#", icon: <CiSearch size={20} /> },
+];
