69bb0c4d6a5a918aea90d0c909602d95760334da
Joshardals <joshuabamidele219@gmail.com>
2024-02-13 22:47:50 +0100

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { kanit, nunito } from "./fonts";
-import "animate.css";
-import "./globals.css";
+// import type { Metadata } from "next";
+// import { kanit, nunito } from "./fonts";
+// import "animate.css";
+// import "./globals.css";
 
-export const metadata: Metadata = {
-  title: "VALORA | Official Store - Shop Online Now",
-  description:
-    "Valora, a personal e-commerce project focusing on clothing, accessories, and gadgets.",
-};
+// export const metadata: Metadata = {
+//   title: "VALORA | Official Store - Shop Online Now",
+//   description:
+//     "Valora, a personal e-commerce project focusing on clothing, accessories, and gadgets.",
+// };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en" className={`${kanit.variable} ${nunito.variable}`}>
-      <body>{children}</body>
-    </html>
-  );
-}
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en" className={`${kanit.variable} ${nunito.variable}`}>
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
