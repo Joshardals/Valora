dba62b3da198bfb6ce0fa713d4c4149a3e362fdb
Joshardals <joshuabamidele219@gmail.com>
2024-02-14 12:54:43 +0100

    initial commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,41 @@
+"use client";
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
+import UserActionsSideBar from "@/components/shared/UserActionsSideBar";
+import UserActionMobileSideBar from "@/components/ui/UserActionSideBar/Mobile/UserActionMobileSideBar";
+import {
+  userActionInitialRender,
+  userActionSideBarToggle,
+} from "@/lib/store/store";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { isOpen, setIsOpen } = userActionSideBarToggle();
+  const { setInitialRender } = userActionInitialRender();
+
+  return (
+    <div
+      className=""
+      onClick={() => {
+        if (isOpen) {
+          setIsOpen(false);
+          setInitialRender(true);
+        }
+      }}
+    >
+      <Header />
+      <UserActionsSideBar />
+      <UserActionMobileSideBar />
+
+      <main className=" bg-gray-950 pt-[4.7rem] px-5 min-h-[100svh]">
+        {children}
+        <SpeedInsights />
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,13 @@ export const metadata: Metadata = {
     "Valora, a personal e-commerce project focusing on clothing, accessories, and gadgets.",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: "0",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
