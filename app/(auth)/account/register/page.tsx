3dd1c5a889301de355d9ac8fe8e26714c5090f20
Joshardals <joshuabamidele219@gmail.com>
2024-02-17 22:16:56 +0100

    initial commit

--- app/(auth)/account/register/page.tsx
+++ app/(auth)/account/register/page.tsx
@@ -1,5 +1,5 @@
-import RegisterForm from "@/components/form/RegisterForm";
 import Link from "next/link";
+import RegisterForm from "@/components/form/RegisterForm";
 
 export default function page() {
   return (
