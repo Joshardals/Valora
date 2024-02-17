add35e4911d24052cace2338ae001b6b06e019b6
Joshardals <joshuabamidele219@gmail.com>
2024-02-17 23:28:34 +0100

    initial commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,27 +1,32 @@
-"use client";
-
+import { Metadata } from "next";
 import {
   userActionInitialRender,
   userActionSideBarToggle,
 } from "@/lib/store/store";
+
+export const metadata: Metadata = {
+  title: "Create Account - VALORA",
+  description:
+    "Valora, a personal e-commerce project focusing on clothing, accessories, and gadgets.",
+};
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { isOpen, setIsOpen } = userActionSideBarToggle();
-  const { setInitialRender } = userActionInitialRender();
+  // const { isOpen, setIsOpen } = userActionSideBarToggle();
+  // const { setInitialRender } = userActionInitialRender();
 
   return (
     <div
       className=""
-      onClick={() => {
-        if (isOpen) {
-          setIsOpen(false);
-          setInitialRender(true);
-        }
-      }}
+      // onClick={() => {
+      //   if (isOpen) {
+      //     setIsOpen(false);
+      //     setInitialRender(true);
+      //   }
+      // }}
     >
       <main className=" bg-gray-950 min-h-[100svh]">{children}</main>
     </div>
