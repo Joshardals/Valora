6b08a87c3c7524c4ab5b3c7a3e66a6768dc775f1
Joshardals <joshuabamidele219@gmail.com>
2024-02-18 13:10:53 +0100

    initial commit

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -14,7 +14,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "bg-red-500 rounded-none capitalize outline-none border-b border-secondary py-2 w-full text-secondary placeholder:text-secondary placeholder:select-none placeholder:text-[14px]",
+          "rounded-none capitalize outline-none border-b border-secondary py-2 w-full text-secondary placeholder:text-secondary placeholder:select-none placeholder:text-[14px]",
           pathname === "/account/register" &&
             "border-primary text-primary placeholder:text-primary",
           type === "email" && "lowercase placeholder:uppercase",
