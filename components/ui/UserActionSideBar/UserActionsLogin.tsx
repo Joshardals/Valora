0fda97039869b239139488a7a3a45d77513a4b62
Joshardals <joshuabamidele219@gmail.com>
2024-02-18 14:31:52 +0100

    initial commit

--- components/ui/UserActionSideBar/UserActionsLogin.tsx
+++ components/ui/UserActionSideBar/UserActionsLogin.tsx
@@ -1,59 +1,59 @@
-// import LoginForm from "@/components/form/LoginForm";
-// import {
-//   userActionActiveIndex,
-//   userActionInitialRender,
-//   userActionSideBarToggle,
-// } from "@/lib/store/store";
-// import Link from "next/link";
-// import { useEffect, useRef } from "react";
+import LoginForm from "@/components/form/LoginForm";
+import {
+  userActionActiveIndex,
+  userActionInitialRender,
+  userActionSideBarToggle,
+} from "@/lib/store/store";
+import Link from "next/link";
+import { useEffect, useRef } from "react";
 
-// export default function UserActionsLogin() {
-//   const { activeIndex } = userActionActiveIndex();
-//   const { initialRender, setInitialRender } = userActionInitialRender();
-//   const { isOpen, setIsOpen } = userActionSideBarToggle();
-//   const profileRef = useRef<HTMLDivElement | null>(null);
+export default function UserActionsLogin() {
+  const { activeIndex } = userActionActiveIndex();
+  const { initialRender, setInitialRender } = userActionInitialRender();
+  const { isOpen, setIsOpen } = userActionSideBarToggle();
+  const profileRef = useRef<HTMLDivElement | null>(null);
 
-//   useEffect(() => {
-//     if (isOpen) {
-//       if (activeIndex === 1 && initialRender) {
-//         profileRef.current?.classList.add("fadeIn");
-//         setInitialRender(false);
-//       } else {
-//         profileRef.current?.classList.remove("fadeIn");
-//       }
-//     }
-//   }, [isOpen, activeIndex]);
+  useEffect(() => {
+    if (isOpen) {
+      if (activeIndex === 1 && initialRender) {
+        profileRef.current?.classList.add("fadeIn");
+        setInitialRender(false);
+      } else {
+        profileRef.current?.classList.remove("fadeIn");
+      }
+    }
+  }, [isOpen, activeIndex]);
 
-//   const handleRegisterClick = () => {
-//     setIsOpen(false);
-//   };
+  const handleRegisterClick = () => {
+    setIsOpen(false);
+  };
 
-//   return (
-//     <div
-//       className={`px-8 space-y-12 ${
-//         isOpen ? "" : isOpen !== null && "fadeOut"
-//       } ${activeIndex === 1 ? "block" : "hidden"}`}
-//       ref={profileRef}
-//     >
-//       <div className="space-y-4">
-//         <h2>Log In</h2>
-//         <p className=" font-light text-pretty max-w-full">
-//           Don&apos;t have an account?{" "}
-//           <span>
-//             <Link
-//               href="/account/register"
-//               className="linkHoverDark linkUnderline"
-//               onClick={handleRegisterClick}
-//             >
-//               Create an account
-//             </Link>{" "}
-//             to unlock your order history, checkout faster, and protect your
-//             Wishlist.
-//           </span>
-//         </p>
-//       </div>
+  return (
+    <div
+      className={`px-8 space-y-12 ${
+        isOpen ? "" : isOpen !== null && "fadeOut"
+      } ${activeIndex === 1 ? "block" : "hidden"}`}
+      ref={profileRef}
+    >
+      <div className="space-y-4">
+        <h2>Log In</h2>
+        <p className=" font-light text-pretty max-w-full">
+          Don&apos;t have an account?{" "}
+          <span>
+            <Link
+              href="/account/register"
+              className="linkHoverDark linkUnderline"
+              onClick={handleRegisterClick}
+            >
+              Create an account
+            </Link>{" "}
+            to unlock your order history, checkout faster, and protect your
+            Wishlist.
+          </span>
+        </p>
+      </div>
 
-//       <LoginForm />
-//     </div>
-//   );
-// }
+      <LoginForm />
+    </div>
+  );
+}
