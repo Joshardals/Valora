667872c29e3d1fddcbce0d05ee24b762af85d7f9
Joshardals <joshuabamidele219@gmail.com>
2024-02-18 12:46:23 +0100

    initial commit

--- app/(auth)/account/register/page.tsx
+++ app/(auth)/account/register/page.tsx
@@ -4,7 +4,7 @@ import RegisterForm from "@/components/form/RegisterForm";
 export default function page() {
   return (
     <section className="grid grid-cols-2 max-md:grid-cols-1 min-h-[100svh]">
-      <div className="bg-secondary pl-28 py-10 text-primary flex justify-between">
+      <div className="bg-secondary md:pl-28 px-5 py-10 text-primary flex justify-between max-md:order-last">
         <div className="space-y-12 flex-1">
           <div className="space-y-8">
             <Link href="/" className="flex">
@@ -19,7 +19,7 @@ export default function page() {
           </div>
           <RegisterForm />
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center max-md:hidden">
           <h2 className="font-playfair -rotate-90 text-3xl select-none">
             Create Account
           </h2>
