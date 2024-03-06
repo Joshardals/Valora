799674cc069efc286aa08167bf7a2b33f2d08019
Joshardals <joshuabamidele219@gmail.com>
2024-03-06 19:42:01 +0100

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,30 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const jwt = require("jsonwebtoken");
+// const express = require("express");
+// const dotenv = require("dotenv");
+// const jwt = require("jsonwebtoken");
 
-const app = express();
+// const app = express();
 
-dotenv.config(); 
-let jwtSecretKey = process.env.JWT_SECRET_KEY; 
-let tokenHeaderKey = process.env.TOKEN_HEADER_KEY; 
+// dotenv.config(); 
+// let jwtSecretKey = process.env.JWT_SECRET_KEY; 
+// let tokenHeaderKey = process.env.TOKEN_HEADER_KEY; 
 
-let PORT = process.env.PORT || 5000; 
-app.listen(PORT, () => {
-  console.log(`Server is up and running on localhost:${PORT} ...`)
-})
+// let PORT = process.env.PORT || 5000; 
+// app.listen(PORT, () => {
+//   console.log(`Server is up and running on localhost:${PORT} ...`)
+// })
 
+// const posts = [
+//   {
+//     username: "Joshardals", 
+//     title: "Python Ninja", 
+//   }, 
+//   {
+//     username: "FireLord", 
+//     title: "ZukoMerchant"
+//   }
+// ]
 
-
+// app.get("/posts", (req, res) => {
+//   res.json(posts)
+// })
 
