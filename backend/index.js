ae4c859037bdd3830962658437b28d1aadb9de5e
Joshardals <joshuabamidele219@gmail.com>
2024-03-06 19:34:24 +0100

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -13,38 +13,6 @@ app.listen(PORT, () => {
   console.log(`Server is up and running on localhost:${PORT} ...`)
 })
 
-// Generating JWT
 
-app.post("/user/generateToken", (req, res) => { 
-  let data = {
-    time: Date(), 
-    userId: 12, 
-  }
 
-  try {
-    const token = jwt.sign(data, jwtSecretKey);
-    res.send(token)
-  } catch (error) {
-    console.log(error); 
-    res.status(500).send("Internal Server Error")
-  }
 
-  res.send(token); 
-})
-
-// Verification of JWT
-
-app.get("/user/validateToken", (req, res) => {
-  try {
-    const token = req.header(tokenHeaderKey); 
-
-    const verified = jwt.verify(token, jwtSecretKey); 
-    if (verified) {
-      return res.send("Successfully Verified");
-    } else {
-      return res.status(401).send(error); 
-    }
-  } catch (error) {
-    return res.status(401).send(error); 
-  }
-})
