e36d66abd35f20ad081f567f0940e213c5bade05
Joshardals <joshuabamidele219@gmail.com>
2024-03-10 14:40:06 +0100

    initial commit

--- components/form/RegisterForm.tsx
+++ components/form/RegisterForm.tsx
@@ -1,140 +1,140 @@
-"use client";
-import Link from "next/link";
-import { Button } from "../ui/button";
-import { Input } from "../ui/input";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
-import { RegisterValidation } from "@/lib/validations/form";
-import { RegisterValidationType } from "@/typings/form";
-import { useForm } from "react-hook-form";
-import { valueWithoutSpaces } from "@/lib/utils";
-import { zodResolver } from "@hookform/resolvers/zod";
+// "use client";
+// import Link from "next/link";
+// import { Button } from "../ui/button";
+// import { Input } from "../ui/input";
+// import {
+//   Form,
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { RegisterValidation } from "@/lib/validations/form";
+// import { RegisterValidationType } from "@/typings/form";
+// import { useForm } from "react-hook-form";
+// import { valueWithoutSpaces } from "@/lib/utils";
+// import { zodResolver } from "@hookform/resolvers/zod";
 
-export default function RegisterForm() {
-  const form = useForm<RegisterValidationType>({
-    resolver: zodResolver(RegisterValidation),
-    defaultValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-    },
-  });
+// export default function RegisterForm() {
+//   const form = useForm<RegisterValidationType>({
+//     resolver: zodResolver(RegisterValidation),
+//     defaultValues: {
+//       firstName: "",
+//       lastName: "",
+//       email: "",
+//       password: "",
+//     },
+//   });
 
-  const onSubmit = async (values: RegisterValidationType) => {
-    alert(values.password);
-  };
+//   const onSubmit = async (values: RegisterValidationType) => {
+//     alert(values.password);
+//   };
 
-  return (
-    <Form {...form}>
-      <form
-      
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-12 max-w-[1200px] mx-auto"
-        autoComplete="off"
-      >
-        <FormField
-          control={form.control}
-          name="firstName"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  autoCapitalize="none"
-                  autoComplete="false"
-                  autoCorrect="off"
-                  id="firstName"
-                  placeholder="FIRST NAME*"
-                  type="text"
-                  {...field}
-                  onChange={(e) => {
-                    form.setValue("firstName", valueWithoutSpaces(e));
-                  }}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
+//   return (
+//     <Form {...form}>
+//       <form
 
-        <FormField
-          control={form.control}
-          name="lastName"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  autoCapitalize="none"
-                  autoComplete="false"
-                  autoCorrect="off"
-                  id="lastName"
-                  placeholder="LAST NAME*"
-                  type="text"
-                  {...field}
-                  onChange={(e) => {
-                    form.setValue("lastName", valueWithoutSpaces(e));
-                  }}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
+//         onSubmit={form.handleSubmit(onSubmit)}
+//         className="space-y-12 max-w-[1200px] mx-auto"
+//         autoComplete="off"
+//       >
+//         <FormField
+//           control={form.control}
+//           name="firstName"
+//           render={({ field }) => (
+//             <FormItem>
+//               <FormControl>
+//                 <Input
+//                   autoCapitalize="none"
+//                   autoComplete="false"
+//                   autoCorrect="off"
+//                   id="firstName"
+//                   placeholder="FIRST NAME*"
+//                   type="text"
+//                   {...field}
+//                   onChange={(e) => {
+//                     form.setValue("firstName", valueWithoutSpaces(e));
+//                   }}
+//                 />
+//               </FormControl>
+//             </FormItem>
+//           )}
+//         />
 
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  autoCapitalize="none"
-                  autoComplete="false"
-                  autoCorrect="off"
-                  id="email"
-                  placeholder="EMAIL*"
-                  type="email"
-                  {...field}
-                  onChange={(e) => {
-                    form.setValue("email", valueWithoutSpaces(e));
-                  }}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
+//         <FormField
+//           control={form.control}
+//           name="lastName"
+//           render={({ field }) => (
+//             <FormItem>
+//               <FormControl>
+//                 <Input
+//                   autoCapitalize="none"
+//                   autoComplete="false"
+//                   autoCorrect="off"
+//                   id="lastName"
+//                   placeholder="LAST NAME*"
+//                   type="text"
+//                   {...field}
+//                   onChange={(e) => {
+//                     form.setValue("lastName", valueWithoutSpaces(e));
+//                   }}
+//                 />
+//               </FormControl>
+//             </FormItem>
+//           )}
+//         />
 
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  autoCapitalize="none"
-                  autoComplete="new-password"
-                  autoCorrect="false"
-                  className="text-[0.6rem]"
-                  id="password"
-                  placeholder="PASSWORD*"
-                  type="password"
-                  {...field}
-                  onChange={(e) => {
-                    form.setValue("password", valueWithoutSpaces(e));
-                  }}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
+//         <FormField
+//           control={form.control}
+//           name="email"
+//           render={({ field }) => (
+//             <FormItem>
+//               <FormControl>
+//                 <Input
+//                   autoCapitalize="none"
+//                   autoComplete="false"
+//                   autoCorrect="off"
+//                   id="email"
+//                   placeholder="EMAIL*"
+//                   type="email"
+//                   {...field}
+//                   onChange={(e) => {
+//                     form.setValue("email", valueWithoutSpaces(e));
+//                   }}
+//                 />
+//               </FormControl>
+//             </FormItem>
+//           )}
+//         />
 
-        <Button variant={"register"} size={"lg"}>
-          Create
-        </Button>
-      </form>
-    </Form>
-  );
-}
+//         <FormField
+//           control={form.control}
+//           name="password"
+//           render={({ field }) => (
+//             <FormItem>
+//               <FormControl>
+//                 <Input
+//                   autoCapitalize="none"
+//                   autoComplete="new-password"
+//                   autoCorrect="false"
+//                   className="text-[0.6rem]"
+//                   id="password"
+//                   placeholder="PASSWORD*"
+//                   type="password"
+//                   {...field}
+//                   onChange={(e) => {
+//                     form.setValue("password", valueWithoutSpaces(e));
+//                   }}
+//                 />
+//               </FormControl>
+//             </FormItem>
+//           )}
+//         />
+
+//         <Button variant={"register"} size={"lg"}>
+//           Create
+//         </Button>
+//       </form>
+//     </Form>
+//   );
+// }
