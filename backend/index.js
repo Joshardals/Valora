81a65f5c7599ed232d58a8abc686e4d75c74d29a
Joshardals <joshuabamidele219@gmail.com>
2024-03-10 14:38:50 +0100

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,26 @@
 
 
-const dotenv = require("dotenv");
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const { PrismaClient } = require("@prisma/client"); 
-const prisma = new PrismaClient(); 
+// const dotenv = require("dotenv");
+// const express = require("express");
+// const jwt = require("jsonwebtoken");
+// const { PrismaClient } = require("@prisma/client"); 
+// const prisma = new PrismaClient(); 
 
-const app = express();
-const bcrypt = require("bcryptjs"); 
+// const app = express();
+// const bcrypt = require("bcryptjs"); 
 
-dotenv.config(); 
+// dotenv.config(); 
 
-let PORT = process.env.PORT || 5000; 
-app.listen(PORT, () => {
-  console.log(`Server is up and running on localhost:${PORT} ...`)
-})
+// let PORT = process.env.PORT || 5000; 
+// app.listen(PORT, () => {
+//   console.log(`Server is up and running on localhost:${PORT} ...`)
+// })
 
-async function main() {
+// async function main() {
 
 
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+//   const allUsers = await prisma.user.findMany()
+//   console.log(allUsers)
   
-}
-main(); 
+// }
+// main(); 
