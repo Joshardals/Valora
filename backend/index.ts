666c5fcdb05d1e3af808b947651a62df3d6ff938
Joshardals <joshuabamidele219@gmail.com>
2024-03-11 19:51:25 +0100

    initial commit

--- backend/index.ts
+++ backend/index.ts
@@ -1,47 +1,47 @@
-const dotenv = require("dotenv");
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+// const dotenv = require("dotenv");
+// const express = require("express");
+// const jwt = require("jsonwebtoken");
+// const { PrismaClient } = require("@prisma/client");
+// const prisma = new PrismaClient();
 
-const app = express();
-const bcrypt = require("bcryptjs");
+// const app = express();
+// const bcrypt = require("bcryptjs");
 
-dotenv.config();
+// dotenv.config();
 
-let PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is up and running on localhost:${PORT} ...`);
-});
+// let PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is up and running on localhost:${PORT} ...`);
+// });
 
-app.get("/api/posts", async (req: any, res: any) => {
-  try {
-    const data = await prisma.post.findMany();
-    console.log(data);
+// app.get("/api/posts", async (req: any, res: any) => {
+//   try {
+//     const data = await prisma.post.findMany();
+//     console.log(data);
 
-    res.status(201).send({ data });
-  } catch (error: any) {
-    console.log(`Error fetching posts: ${error.message}`);
-    res.status(500).send({
-      message: "Error fetching posts",
-    });
-  }
-});
+//     res.status(201).send({ data });
+//   } catch (error: any) {
+//     console.log(`Error fetching posts: ${error.message}`);
+//     res.status(500).send({
+//       message: "Error fetching posts",
+//     });
+//   }
+// });
 
-app.post("/api/createPost", express.json(), async (req: any, res: any) => {
-  console.log(req.body);
-  try {
-    const { title, content } = await req.body;
-    await prisma.post.create({
-      data: {
-        title,
-        content,
-      },
-    });
-  } catch (error: any) {
-    console.log(`Error fetching posts: ${error.message}`);
-    res.status(500).send({
-      message: "Error fetching posts",
-    });
-  }
-});
+// app.post("/api/createPost", express.json(), async (req: any, res: any) => {
+//   console.log(req.body);
+//   try {
+//     const { title, content } = await req.body;
+//     await prisma.post.create({
+//       data: {
+//         title,
+//         content,
+//       },
+//     });
+//   } catch (error: any) {
+//     console.log(`Error fetching posts: ${error.message}`);
+//     res.status(500).send({
+//       message: "Error fetching posts",
+//     });
+//   }
+// });
