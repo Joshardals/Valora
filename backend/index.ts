81943bbc26d22a8e38f96d8f50972106d39c0ee5
Joshardals <joshuabamidele219@gmail.com>
2024-03-11 21:42:51 +0100

    initial commit

--- backend/index.ts
+++ backend/index.ts
@@ -1,58 +1,38 @@
-const express = require("express");
-const app = express();
-const bcrypt = require("bcryptjs");
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+// const express = require("express");
+// const app = express();
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+// const { PrismaClient } = require("@prisma/client");
+// const prisma = new PrismaClient();
 
-// Creating a function to hash password using bcrypt
-async function hashPassword(password: string) {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
-}
+// // Creating a function to hash password using bcrypt
+// async function hashPassword(password: string) {
+//   const salt = await bcrypt.genSalt(10);
+//   return await bcrypt.hash(password, salt);
+// }
 
-app.use(cors);
+// app.post("/api/register", express.json(), async (req: any, res: any) => {
+//   try {
+//     const { firstName, lastName, email, password } = req.body;
+//     const hashedPassword = await hashPassword(password);
 
-app.use(function (req: any, res: any, next: any) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin,Cache-Control,Accept,X-Access-Token ,X-Requested-With, Content-Type, Access-Control-Request-Method"
-  );
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-  next();
-});
+//     await prisma.user.create({
+//       data: {
+//         firstName,
+//         lastName,
+//         email,
+//         password: hashedPassword,
+//       },
+//     });
 
-app.post("/api/register", express.json(), async (req: any, res: any) => {
-  try {
-    const { firstName, lastName, email, password } = req.body;
-    const hashedPassword = await hashPassword(password);
+//     res.status(201).json({ message: "User created successfully" });
+//   } catch (error: any) {
+//     console.log(`Error creating user: ${error.message}`);
+//     res.status(500).json({ message: "Error creating user" });
+//   }
+// });
 
-    await prisma.user.create({
-      data: {
-        firstName,
-        lastName,
-        email,
-        password: hashedPassword,
-      },
-    });
-
-    res.status(201).json({ message: "User created successfully" });
-  } catch (error: any) {
-    console.log(`Error creating user: ${error.message}`);
-    res.status(500).json({ message: "Error creating user" });
-  }
-});
-
-let PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is up and running on localhost:${PORT} ...`);
-});
+// let PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is up and running on localhost:${PORT} ...`);
+// });
