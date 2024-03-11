cc3f881d62ed0229a11ebad5608b45b6f7505442
Joshardals <joshuabamidele219@gmail.com>
2024-03-11 21:22:19 +0100

    initial commit

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,35 @@
 const express = require("express");
+const app = express();
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-
-const app = express();
-const bcrypt = require("bcryptjs");
 
 // Creating a function to hash password using bcrypt
 async function hashPassword(password: string) {
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
 }
+
+app.use(cors);
+
+app.use(function (req: any, res: any, next: any) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,Cache-Control,Accept,X-Access-Token ,X-Requested-With, Content-Type, Access-Control-Request-Method"
+  );
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  next();
+});
 
 app.post("/api/register", express.json(), async (req: any, res: any) => {
   try {
