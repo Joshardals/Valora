c029b0addf8aaa14633cb5791e52c8acff228f1b
Joshardals <joshuabamidele219@gmail.com>
2024-03-11 20:05:35 +0100

    initial commit

--- backend/index.ts
+++ backend/index.ts
@@ -9,39 +9,19 @@ const bcrypt = require("bcryptjs");
 
 dotenv.config();
 
+app.post("/api/register", express.json(), async (req: any, res: any) => {
+  try {
+    const { firstName, lastName, email, password } = await req.body;
+    // await prisma.post.create({
+    //   data: {
+    //     title,
+    //     content,
+    //   },
+    // });
+  } catch (error: any) {}
+});
+
 let PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is up and running on localhost:${PORT} ...`);
-});
-
-app.get("/api/posts", async (req: any, res: any) => {
-  try {
-    const data = await prisma.post.findMany();
-    console.log(data);
-
-    res.status(201).send({ data });
-  } catch (error: any) {
-    console.log(`Error fetching posts: ${error.message}`);
-    res.status(500).send({
-      message: "Error fetching posts",
-    });
-  }
-});
-
-app.post("/api/createPost", express.json(), async (req: any, res: any) => {
-  console.log(req.body);
-  try {
-    const { title, content } = await req.body;
-    await prisma.post.create({
-      data: {
-        title,
-        content,
-      },
-    });
-  } catch (error: any) {
-    console.log(`Error fetching posts: ${error.message}`);
-    res.status(500).send({
-      message: "Error fetching posts",
-    });
-  }
 });
