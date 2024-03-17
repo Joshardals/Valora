157306bd1966cc797e92d68a2b217e00874d56ea
Joshardals <joshuabamidele219@gmail.com>
2024-03-17 15:59:05 +0100

    initial commit

--- backend/index.ts
+++ backend/index.ts
@@ -1,42 +1,42 @@
-const express = require("express");
-const app = express();
-const bcrypt = require("bcryptjs");
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+// const express = require("express");
+// const app = express();
+// const bcrypt = require("bcryptjs");
+// const cors = require("cors");
+// const jwt = require("jsonwebtoken");
+// const { PrismaClient } = require("@prisma/client");
+// const prisma = new PrismaClient();
 
-app.use(cors());
+// app.use(cors());
 
-// Creating a function to hash password using bcrypt
-async function hashPassword(password: string) {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
-}
+// // Creating a function to hash password using bcrypt
+// async function hashPassword(password: string) {
+//   const salt = await bcrypt.genSalt(10);
+//   return await bcrypt.hash(password, salt);
+// }
 
-// A post request for the register an account page.
-app.post("/api/v1/register", express.json(), async (req: any, res: any) => {
-  try {
-    const { firstName, lastName, email, password } = req.body;
-    const hashedPassword = await hashPassword(password);
+// // A post request for the register an account page.
+// app.post("/api/v1/register", express.json(), async (req: any, res: any) => {
+//   try {
+//     const { firstName, lastName, email, password } = req.body;
+//     const hashedPassword = await hashPassword(password);
 
-    await prisma.user.create({
-      data: {
-        firstName,
-        lastName,
-        email,
-        password: hashedPassword,
-      },
-    });
+//     await prisma.user.create({
+//       data: {
+//         firstName,
+//         lastName,
+//         email,
+//         password: hashedPassword,
+//       },
+//     });
 
-    res.status(201).json({ message: "User created successfully" });
-  } catch (error: any) {
-    console.log(`Error creating user: ${error.message}`);
-    res.status(500).json({ message: "Error creating user" });
-  }
-});
+//     res.status(201).json({ message: "User created successfully" });
+//   } catch (error: any) {
+//     console.log(`Error creating user: ${error.message}`);
+//     res.status(500).json({ message: "Error creating user" });
+//   }
+// });
 
-let PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is up and running on localhost:${PORT} ...`);
-});
+// let PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is up and running on localhost:${PORT} ...`);
+// });
