19b46eb4fe06138c3d81ac166a464c06b94019e5
Joshardals <joshuabamidele219@gmail.com>
2024-03-20 14:28:42 +0100

    initial commit

--- components/form/LoginForm.tsx
+++ components/form/LoginForm.tsx
@@ -1,111 +1,111 @@
-// import { Button } from "../ui/button";
-// import { ChangeEvent } from "react";
-// import { firstCaseUpper, valueWithoutSpaces } from "@/lib/utils";
-// import {
-//   Form,
-//   FormControl,
-//   FormField,
-//   FormItem,
-//   FormMessage,
-// } from "@/components/ui/form";
-// import { Input } from "../ui/input";
-// import Link from "next/link";
-// import { fetchUserData, loginUser } from "@/lib/actions/auth/auth.action";
-// import { SignUpValidation } from "@/lib/validations/form";
-// import { SignUpValidationType } from "@/typings/form";
-// import { useForm } from "react-hook-form";
-// import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "../ui/button";
+import { ChangeEvent } from "react";
+import { firstCaseUpper, valueWithoutSpaces } from "@/lib/utils";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "../ui/input";
+import Link from "next/link";
+import { fetchUserData, loginUser } from "@/lib/actions/auth/auth.action";
+import { SignUpValidation } from "@/lib/validations/form";
+import { SignUpValidationType } from "@/typings/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-// export default function LoginForm() {
-//   const form = useForm<SignUpValidationType>({
-//     resolver: zodResolver(SignUpValidation),
-//     defaultValues: {
-//       email: "",
-//       password: "",
-//     },
-//   });
+export default function LoginForm() {
+  const form = useForm<SignUpValidationType>({
+    resolver: zodResolver(SignUpValidation),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-//   const onSubmit = async (values: SignUpValidationType) => {
-//     try {
-//       await loginUser({
-//         email: values.email,
-//         password: values.password,
-//       });
+  const onSubmit = async (values: SignUpValidationType) => {
+    try {
+      await loginUser({
+        email: values.email,
+        password: values.password,
+      });
 
-//       const users = await fetchUserData();
+      const users = await fetchUserData();
 
-//       console.log(users);
-//     } catch (error: any) {
-//       console.log(`Invalid Email or Password: ${error}`);
-//     }
+      console.log(users);
+    } catch (error: any) {
+      console.log(`Invalid Email or Password: ${error}`);
+    }
 
-//     form.setValue("email", "");
-//     form.setValue("password", "");
-//   };
+    form.setValue("email", "");
+    form.setValue("password", "");
+  };
 
-//   return (
-//     <Form {...form}>
-//       <form
-//         onSubmit={form.handleSubmit(onSubmit)}
-//         className="space-y-12"
-//         autoComplete="off"
-//       >
-//         <FormField
-//           control={form.control}
-//           name="email"
-//           render={({ field }) => (
-//             <FormItem>
-//               <FormControl>
-//                 <Input
-//                   autoCapitalize="none"
-//                   autoComplete="false"
-//                   autoCorrect="off"
-//                   id="email"
-//                   placeholder="EMAIL*"
-//                   type="email"
-//                   {...field}
-//                   onChange={(e) => {
-//                     form.setValue("email", e.target.value);
-//                   }}
-//                 />
-//               </FormControl>
-//             </FormItem>
-//           )}
-//         />
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-12"
+        autoComplete="off"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  autoCapitalize="none"
+                  autoComplete="false"
+                  autoCorrect="off"
+                  id="email"
+                  placeholder="EMAIL*"
+                  type="email"
+                  {...field}
+                  onChange={(e) => {
+                    form.setValue("email", e.target.value);
+                  }}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
 
-//         <FormField
-//           control={form.control}
-//           name="password"
-//           render={({ field }) => (
-//             <FormItem>
-//               <FormControl>
-//                 <Input
-//                   autoCapitalize="none"
-//                   autoComplete="new-password"
-//                   autoCorrect="false"
-//                   className="text-[0.6rem]"
-//                   id="password"
-//                   placeholder="PASSWORD*"
-//                   type="password"
-//                   {...field}
-//                   onChange={(e) => {
-//                     const processedValue = valueWithoutSpaces(e.target.value);
-//                     form.setValue("password", processedValue);
-//                   }}
-//                 />
-//               </FormControl>
-//             </FormItem>
-//           )}
-//         />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  autoCapitalize="none"
+                  autoComplete="new-password"
+                  autoCorrect="false"
+                  className="text-[0.6rem]"
+                  id="password"
+                  placeholder="PASSWORD*"
+                  type="password"
+                  {...field}
+                  onChange={(e) => {
+                    const processedValue = valueWithoutSpaces(e.target.value);
+                    form.setValue("password", processedValue);
+                  }}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
 
-//         <Button>Login</Button>
+        <Button>Login</Button>
 
-//         <Link href="/register" className="flex justify-center w-full">
-//           <p className="uppercase md:linkHoverDark linkUnderline decoration-secondar font-light">
-//             forgotten password
-//           </p>
-//         </Link>
-//       </form>
-//     </Form>
-//   );
-// }
+        <Link href="/register" className="flex justify-center w-full">
+          <p className="uppercase md:linkHoverDark linkUnderline decoration-secondar font-light">
+            forgotten password
+          </p>
+        </Link>
+      </form>
+    </Form>
+  );
+}
