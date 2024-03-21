f2cf53d44f07e68fc86553ea6bbe19e07718539d
Joshardals <joshuabamidele219@gmail.com>
2024-03-21 18:28:20 +0100

    initial commit

--- backend/index.ts
+++ backend/index.ts
@@ -1,108 +1,108 @@
-const express = require("express");
-const app = express();
-const bcrypt = require("bcryptjs");
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+// const express = require("express");
+// const app = express();
+// const bcrypt = require("bcryptjs");
+// const cors = require("cors");
+// const jwt = require("jsonwebtoken");
+// const { PrismaClient } = require("@prisma/client");
+// const prisma = new PrismaClient();
 
-app.use(cors());
+// app.use(cors());
 
-// Creating a function to hash password using bcrypt
-async function hashPassword(password: string) {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
-}
+// // Creating a function to hash password using bcrypt
+// async function hashPassword(password: string) {
+//   const salt = await bcrypt.genSalt(10);
+//   return await bcrypt.hash(password, salt);
+// }
 
-const generateToken = (user: { id: number }) => {
-  const payload = {
-    userId: user.id, // Include other relevant user data if needed
-  };
+// const generateToken = (user: { id: number }) => {
+//   const payload = {
+//     userId: user.id, // Include other relevant user data if needed
+//   };
 
-  const options = {
-    expiresIn: "30m", // Set token expiry time (e.g., 30 minutes)
-    privateKey: process.env.JWT_SECRET,
-  };
+//   const options = {
+//     expiresIn: "30m", // Set token expiry time (e.g., 30 minutes)
+//     privateKey: process.env.JWT_SECRET,
+//   };
 
-  return jwt.sign(payload, options.privateKey, {
-    expiresIn: options.expiresIn,
-  });
-};
+//   return jwt.sign(payload, options.privateKey, {
+//     expiresIn: options.expiresIn,
+//   });
+// };
 
-// A POST request for the register an account page.
-app.post("/api/v1/register", express.json(), async (req: any, res: any) => {
-  try {
-    const { firstName, lastName, email, password } = req.body;
-    const hashedPassword = await hashPassword(password);
+// // A POST request for the register an account page.
+// app.post("/api/v1/register", express.json(), async (req: any, res: any) => {
+//   try {
+//     const { firstName, lastName, email, password } = req.body;
+//     const hashedPassword = await hashPassword(password);
 
-    await prisma.user.create({
-      data: {
-        firstName,
-        lastName,
-        email,
-        password: hashedPassword,
-      },
-    });
+//     await prisma.user.create({
+//       data: {
+//         firstName,
+//         lastName,
+//         email,
+//         password: hashedPassword,
+//       },
+//     });
 
-    res.status(200).json({ message: "User created successfully" });
-  } catch (error: any) {
-    console.log(`Error creating user: ${error.message}`);
-    res.status(500).json({ message: "Error creating user" });
-  }
-});
+//     res.status(200).json({ message: "User created successfully" });
+//   } catch (error: any) {
+//     console.log(`Error creating user: ${error.message}`);
+//     res.status(500).json({ message: "Error creating user" });
+//   }
+// });
 
-// A POST request for login
-app.post("/api/v1/login", express.json(), async (req: any, res: any) => {
-  const { email, password } = req.body;
-  const user = await prisma.user.findUnique({ where: { email } });
+// // A POST request for login
+// app.post("/api/v1/login", express.json(), async (req: any, res: any) => {
+//   const { email, password } = req.body;
+//   const user = await prisma.user.findUnique({ where: { email } });
 
-  if (!user) {
-    return res.status(401).json({ message: "Invalid email or password" });
-  }
+//   if (!user) {
+//     return res.status(401).json({ message: "Invalid email or password" });
+//   }
 
-  const isPasswordValid = await bcrypt.compare(password, user.password);
+//   const isPasswordValid = await bcrypt.compare(password, user.password);
 
-  if (isPasswordValid) {
-    const token = generateToken(user);
-    res.status(200).json({ message: "Login successful", token });
-  } else {
-    res.status(401).json({ message: "Invalid email or password" });
-  }
-});
+//   if (isPasswordValid) {
+//     const token = generateToken(user);
+//     res.status(200).json({ message: "Login successful", token });
+//   } else {
+//     res.status(401).json({ message: "Invalid email or password" });
+//   }
+// });
 
-// A GET request to fetch user data
-app.get("/api/v1/user", express.json(), async (req: any, res: any) => {
-  try {
-    // Get the token from the request headers
-    const token = req.headers.authorization;
+// // A GET request to fetch user data
+// app.get("/api/v1/user", express.json(), async (req: any, res: any) => {
+//   try {
+//     // Get the token from the request headers
+//     const token = req.headers.authorization;
 
-    // Check if the token exists
-    if (!token) {
-      return res.status(401).json({ message: "Unauthorized: Missing token" });
-    }
+//     // Check if the token exists
+//     if (!token) {
+//       return res.status(401).json({ message: "Unauthorized: Missing token" });
+//     }
 
-    const tokenParts = token.split(" ");
-    const tokenValue = tokenParts[1];
+//     const tokenParts = token.split(" ");
+//     const tokenValue = tokenParts[1];
 
-    // Verify the token
-    jwt.verify(tokenParts, process.env.JWT_SECRET, async (err: any) => {
-      if (err) {
-        return res.status(401).json({ message: "Unauthorized: Invalid token" });
-      }
+//     // Verify the token
+//     jwt.verify(tokenParts, process.env.JWT_SECRET, async (err: any) => {
+//       if (err) {
+//         return res.status(401).json({ message: "Unauthorized: Invalid token" });
+//       }
 
-      // Token is valid, fetch user data
-      // const users = await prisma.user.findUnique({ where: { email } });
-      const users = await prisma.user.findMany();
-      res.status(200).json({ users });
-    });
-  } catch (error: any) {
-    console.error("Error fetching user data:", error.message);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
+//       // Token is valid, fetch user data
+//       // const users = await prisma.user.findUnique({ where: { email } });
+//       const users = await prisma.user.findMany();
+//       res.status(200).json({ users });
+//     });
+//   } catch (error: any) {
+//     console.error("Error fetching user data:", error.message);
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// });
 
-// Run Server
-let PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is up and running on localhost:${PORT} ...`);
-});
+// // Run Server
+// let PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is up and running on localhost:${PORT} ...`);
+// });
