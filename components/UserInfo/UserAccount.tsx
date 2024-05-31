933e07ce6c188714d31f34680617808139f30133
Joshardals <joshuabamidele219@gmail.com>
2024-05-31 20:26:28 +0100

    initial commit

--- components/UserInfo/UserAccount.tsx
+++ components/UserInfo/UserAccount.tsx
@@ -15,12 +15,12 @@ export default function UserAccount() {
       }
     };    
   
-    getUserData();
+    getUserData();  
   }, [userId]);
   return (    
     <div>   
       <div className="space-y-4">
-        <h2 className=" font-playfair">Hello {userId}</h2>  
+        <h2 className=" font-playfair">Hello {userId}</h2>    
         <p className="uppercase">Welcome to your account</p>
       </div>
     </div>
