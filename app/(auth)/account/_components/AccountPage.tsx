9a1d76d05c28967c081331de5d171f962ea62475
Joshardals <joshuabamidele219@gmail.com>
2024-07-07 18:22:43 +0100

    initial commit

--- app/(auth)/account/_components/AccountPage.tsx
+++ app/(auth)/account/_components/AccountPage.tsx
@@ -3,43 +3,55 @@ import { auth, db } from "@/lib/firebase/clientFirebase";
 import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import Header from "@/components/shared/Header";
 import Link from "next/link";
+import { signOut } from "firebase/auth";
 import {
   userActionInitialRender,
   userActionSideBarToggle,
 } from "@/lib/store/store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import UserActionsSideBar from "@/components/shared/UserActionsSideBar";
 import UserActionMobileSideBar from "@/components/ui/UserActionSideBar/Mobile/UserActionMobileSideBar";
+import { useRouter } from "next/navigation";
 // import { logoutUser } from "@/lib/actions/auth/auth.action";
 
 export function AccountPage() {
+  const [firstName, setFirstName] = useState<String>();
   const { isOpen, setIsOpen } = userActionSideBarToggle();
+  const router = useRouter();
   const { setInitialRender } = userActionInitialRender();
   const userId = auth.currentUser?.uid || "";
 
   console.log(userId);
 
   const handleLogout = async () => {
-    // await logoutUser();
+    try {
+      await signOut(auth);
+      console.log("User Sign-Out Successful");
+      router.push("/");
+    } catch (error: any) {
+      console.error(`Error Logging Out: ${error.message}`);
+    }
   };
 
   useEffect(() => {
     const getUser = async () => {
-      const userDocRef = doc(db, "users", userId);
+      try {
+        const userDocRef = doc(db, "users", userId);
+        const userDocSnap = await getDoc(userDocRef);
 
-      onSnapshot(userDocRef, (doc) => {
-        if (doc.exists()) {
-          const userData = doc.data();
-
-          console.log(userData);
+        if (userDocSnap.exists()) {
+          const userData = userDocSnap.data();
+          setFirstName(userData.firstName);
         } else {
           console.log("no-data");
         }
-      });
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
     };
 
     getUser();
-  }, [userId]);
+  }, []);
   return (
     <div
       onClick={() => {
@@ -56,7 +68,7 @@ export function AccountPage() {
       <div className="bg-primary w-screen h-screen flex items-center pt-[4.7rem] px-12 max-md:px-5">
         <div className="w-full">
           <div className="space-y-4">
-            <h2 className=" font-playfair">Hello Anon</h2>
+            <h2 className=" font-playfair">Hello {firstName}</h2>
             <div className="flex md:space-x-[10rem] max-md:justify-between">
               <p className="uppercase">Welcome to your account</p>
               <p className="underline cursor-pointer" onClick={handleLogout}>
