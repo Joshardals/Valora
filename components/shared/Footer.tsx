91dfcc50258b730aeefb917f26ef92491f7e66e5
Joshardals <joshuabamidele219@gmail.com>
2024-07-12 23:03:42 +0100

    initial commit

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -2,18 +2,20 @@ import WaitListForm from "../form/WaitListForm";
 
 export default function Footer() {
   return (
-    <div className="bg-secondary h-[85vh] px-8 py-20 text-primary flex max-lg:flex-col justify-between">
-      <div className="flex-1 relative">
-        <h1 className="font-playfair text-4xl font-light tracking-wider lg:absolute lg:bottom-0">
-          A brand of the <br /> Transformative Kind.
-        </h1>
-      </div>
-      <div className="flex-1">
-        <div className="flex flex-col h-full pr-40">
-          <h1 className="flex-1">NavLinks - Coming Soon!</h1>
-          <div className="space-y-12">
-            <h2 className="font-light">Stay in touch</h2>
-            <WaitListForm />
+    <div className="bg-secondary">
+      <div className="max-w-[1200px] px-8 lg:py-20 text-primary flex max-lg:flex-col justify-center mx-auto">
+        <div className="flex-1 relative">
+          <h1 className="font-playfair text-4xl font-light tracking-wider lg:absolute lg:bottom-0">
+            A brand of the <br /> Transformative Kind.
+          </h1>
+        </div>
+        <div className="flex-1">
+          <div className="flex flex-col h-full pr-40">
+            <h1 className="flex-1">NavLinks - Coming Soon!</h1>
+            <div className="space-y-12">
+              <h2 className="font-light">Stay in touch</h2>
+              <WaitListForm />
+            </div>
           </div>
         </div>
       </div>
