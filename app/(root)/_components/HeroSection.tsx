07b6faccdfc3400e0fed23c1f04a48481dcaf24b
Joshardals <joshuabamidele219@gmail.com>
2024-07-13 18:17:48 +0100

    initial commit

--- app/(root)/_components/HeroSection.tsx
+++ app/(root)/_components/HeroSection.tsx
@@ -1,13 +1,23 @@
+import Link from "next/link";
+
 export default function HeroSection() {
   return (
-    <div className="bgShadeLight bg-[url('/assets/HeroImg.jpg')] bg-cover w-full h-screen pt-[4.75rem] flex items-end lg:items-center">
+    <div className="bgShadeLight bg-[url('/assets/HeroImg.jpg')] bg-cover w-full max-lg:h-[90vh] h-screen pt-[4.75rem] flex items-end lg:items-center">
       <div className="max-w-[1200px] mx-auto w-full p-5 space-y-4 z-10">
         <h1 className="font-playfair text-3xl font-light tracking-wider capitalize">
           back in stock
         </h1>
-        <p className=" text-lg">
-          A fusion of sophisticated suits and effortless styles.
-        </p>
+        <div className="max-lg:flex max-lg:items-baseline">
+          <p className="max-lg:flex-1 text-lg font-light mb-4">
+            A fusion of sophisticated suits and effortless styles.
+          </p>
+
+          <Link href="#">
+            <p className="uppercase md:linkHoverDark linkUnderline font-light w-[fit-content]">
+              shop now
+            </p>
+          </Link>
+        </div>
       </div>
     </div>
   );
