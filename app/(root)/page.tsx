f79ff37629f1ba8d80bf10876cafaac697ffde40
Joshardals <joshuabamidele219@gmail.com>
2024-07-19 21:00:40 +0100

    Initial commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,9 +13,12 @@ export default async function Home() {
       </section>
       <section className="grid lg:grid-cols-2 gap-2">
         <div className="bg-primary h-screen">1</div>
-        <div className="">
-          <div className="max-w-md bg-primary mx-auto">
-            Some Content Goes Here
+        <div className="p-5 md:py-20 bg-white">
+          <div className="md:max-w-lg md:mx-auto text-primary space-y-8">
+            <p className="uppercase">tatiana korasakova</p>
+            <h1 className="font-playfair text-3xl">
+              Miami Summer – Discover the latest Founders Edit
+            </h1>
           </div>
         </div>
       </section>
