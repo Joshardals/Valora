179fa19fdcbe616c094e2ea56c15c0d54a41746b
Joshardals <joshuabamidele219@gmail.com>
2024-07-23 20:36:03 +0100

    initial commit

--- app/(auth)/account/page.tsx
+++ app/(auth)/account/page.tsx
@@ -1,38 +1,44 @@
-"use client";
-import { AccountPage } from "./_components/AccountPage";
-import { currentUser } from "@/lib/actions/auth/auth.action";
-import { Loading } from "./_components/Loading";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+// "use client";
+// import { AccountPage } from "./_components/AccountPage";
+// import { currentUser } from "@/lib/actions/auth/auth.action";
+// import { Loading } from "./_components/Loading";
+// import { useEffect, useState } from "react";
+// import { useRouter } from "next/navigation";
+
+// export default function UserAccountPage() {
+//   const [authUser, setAuthUser] = useState<boolean>(false);
+//   const [userId, setUserId] = useState<string>();
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const getUser = async () => {
+//       try {
+//         const user = await currentUser();
+//         if (!user) {
+//           router.push("/");
+//           setAuthUser(false);
+//         } else {
+//           setAuthUser(true);
+//           setUserId(user);
+//         }
+//       } catch (error: any) {
+//         console.log(`Error fetching User... ${error.message}`);
+//       }
+//     };
+
+//     getUser();
+//   }, [router]);
+
+//   return (
+//     <>
+//       {authUser ? null : <Loading />}
+//       {authUser ? <AccountPage userId={userId!} /> : null}
+//     </>
+//   );
+// }
+
+import React from "react";
 
 export default function UserAccountPage() {
-  const [authUser, setAuthUser] = useState<boolean>(false);
-  const [userId, setUserId] = useState<string>();
-  const router = useRouter();
-
-  useEffect(() => {
-    const getUser = async () => {
-      try {
-        const user = await currentUser();
-        if (!user) {
-          router.push("/");
-          setAuthUser(false);
-        } else {
-          setAuthUser(true);
-          setUserId(user);
-        }
-      } catch (error: any) {
-        console.log(`Error fetching User... ${error.message}`);
-      }
-    };
-
-    getUser();
-  }, [router]);
-
-  return (
-    <>
-      {authUser ? null : <Loading />}
-      {authUser ? <AccountPage userId={userId!} /> : null}
-    </>
-  );
+  return <div>page</div>;
 }
